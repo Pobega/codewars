95e157a19b0b70cd522843cc8121538e741cd66e
Michael Pobega <pobega@gmail.com>
2020-02-10 12:47:21 -0500

    phone_num: Implement it, I guess?

--- phone_num/src/lib.rs
+++ phone_num/src/lib.rs
@@ -1,5 +1,17 @@
 fn create_phone_number(numbers: &[u8]) -> String {
-    String::new()
+    format!(
+        "({}{}{}) {}{}{}-{}{}{}{}",
+        numbers[0],
+        numbers[1],
+        numbers[2],
+        numbers[3],
+        numbers[4],
+        numbers[5],
+        numbers[6],
+        numbers[7],
+        numbers[8],
+        numbers[9]
+    )
 }
 
 #[cfg(test)]
@@ -8,8 +20,17 @@ mod tests {
 
     #[test]
     fn returns_expected() {
-        assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
-        assert_eq!(create_phone_number(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
-        assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]), "(123) 456-7899");
+        assert_eq!(
+            create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
+            "(123) 456-7890"
+        );
+        assert_eq!(
+            create_phone_number(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+            "(111) 111-1111"
+        );
+        assert_eq!(
+            create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]),
+            "(123) 456-7899"
+        );
     }
 }
