f4002cf02c8f0dbdc799c0d8ecfb0d47f9594d53
Michael Pobega <pobega@gmail.com>
2020-02-10 12:27:25 -0500

    phone_num: fix test syntax

--- phone_num/src/lib.rs
+++ phone_num/src/lib.rs
@@ -1,10 +1,15 @@
 fn create_phone_number(numbers: &[u8]) -> String {
+    String::new()
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
+    fn returns_expected() {
         assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
         assert_eq!(create_phone_number(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
         assert_eq!(create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]), "(123) 456-7899");
+    }
 }
